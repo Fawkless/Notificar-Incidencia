58d5707dd39ee2ecd92d6780bc646013467401e1
Fawkless <118135253+Fawkless@users.noreply.github.com>
2022-11-14 00:06:16 -0300

    primer commit

---
+++ procesar.incidencia.php
@@ -0,0 +1,13 @@
+<?php
+
+$usuario = $_POST['usuario'];
+$departamento = $_POST['departamento'];
+$descripción = $_POST['descripcion'];
+$contraseña = $_POST ['contraseña'];
+
+echo "<p> Usuario : $usuario</p>";
+echo "<p> Departamento : $departamento</p>";
+echo "<p> Descripción: $descripción</p>";
+echo "<p> Contraseña : $contraseña</p>";
+
+?>
